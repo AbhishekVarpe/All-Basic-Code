2b77853438521c09d23fb1aceadc65ec04b96fe4
HP <HP@192.168.0.162>
2025-07-29 13:00:02 +0530

    Add middle element code

--- src/in/com/LinkedListAddFirst.java
+++ src/in/com/LinkedListAddFirst.java
@@ -48,15 +48,33 @@ public class LinkedListAddFirst {
 			System.out.print(temp.data+" ");
 			temp=temp.next;
 		}
+		System.out.println();
+	}
+	public void AddMiddle(int index,int data) {
+		Node nn=new Node(data);
+		Node tempNode=head;
+		int i=0;
+		while(i<index-1) {
+			tempNode=tempNode.next;
+			i++;
+		}
+		nn.next=tempNode.next;
+		tempNode.next=nn;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedListAddFirst linkedListAddFirst=new LinkedListAddFirst();
+		printData();
 		linkedListAddFirst.addFirst(2);
+		printData();
 		linkedListAddFirst.addFirst(1);
+		printData();
 		linkedListAddFirst.Addlast(3);
+		printData();
 		linkedListAddFirst.Addlast(4);
+		printData();
+		linkedListAddFirst.AddMiddle(2, 1000);
 		printData();
 
 	}
