ec63c99f522279fc531a7d7144734489c06f70e9
HP <HP@192.168.0.162>
2025-07-29 11:27:41 +0530

    some change

--- src/in/com/LinkedListAddFirst.java
+++ src/in/com/LinkedListAddFirst.java
@@ -40,6 +40,10 @@ public class LinkedListAddFirst {
 	}
 	public static void printData() {
 		Node temp=head;
+		if(temp==null) {
+			System.out.println("LL is empty");
+			return;
+		}
 		while(temp!=null) {
 			System.out.print(temp.data+" ");
 			temp=temp.next;
